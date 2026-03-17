0167c4a11515b1a1547cf5eeca500e6ad181d7f7
gustavobatista0978 <gustavobatista0978@gmail.com>
2026-03-17 13:56:33 -0300

    commit2

---
+++ LPRaula4/numero_par/numero_par/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite um numero inteiro ");
+int numero = int.Parse(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"{numero} é par");
+}
+else
+{
+    Console.WriteLine($"{numero} é ímpar");
+}
---
+++ LPRaula4/rpg/Rpg/Rpg/Program.cs
@@ -0,0 +1,28 @@
+﻿string escolha = "";
+
+Console.WriteLine("Bem-vindo ao RPG!");
+Console.WriteLine("Escolha sua classe:");
+Console.WriteLine("\n [1] Guerreiro\n [2] Mago\n [3] Arqueira");
+escolha = Console.ReadLine();
+
+switch (escolha)
+{
+    case "1":
+        Console.WriteLine("Você escolheu a classe Guerreiro!");
+        Console.WriteLine("O Guerreiro é forte e resistente, especializado em combate corpo a corpo.");
+        break;
+    case "2":
+        Console.WriteLine("Você escolheu a classe Mago!");
+        Console.WriteLine("O Mago é inteligente e poderoso, especializado em magia e feitiços.");
+        break;
+    case "3":
+        Console.WriteLine("Você escolheu a classe Arqueira!");
+        Console.WriteLine("O Arqueiro é ágil e precisa, especializada em ataques à distância com arco e flecha.");
+        break;
+    default:
+        Console.WriteLine("Opção inválida. Por favor, escolha uma classe válida.");
+        break;
+}
+    
+
+    
