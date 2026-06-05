e938b5c1bbf911002f45b1ca103b736c9e034ce9
gustavo <gustavobatista0978@gmail.com>
2026-06-05 10:36:08 -0300

    aula2

--- LPRAula2/diferença/diferença.cpp
+++ LPRAula2/diferença/diferença.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Digite 4 valores inteiros: " << std::endl;    
+	std::cout << "Digite seus 4 valores inteiros: " << std::endl;    
 	int a, b, c, d;
 	std::cin >> a >> b >> c >> d;
 	int diferenca = (a * b - c * d);
