2f0836d7d137138f546e892bfbd0ee6bc43352a6
gustavobatista0978 <gustavobatista0978@gmail.com>
2026-06-23 08:10:53 -0300

    aula8

--- LPRaula4/par_impar/obj/Debug/net9.0/par_impar.AssemblyInfo.cs
+++ LPRaula4/par_impar/obj/Debug/net9.0/par_impar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("par_impar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd268aeef1bef9521388bba7dfc3b3d9f2f53d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de244c5fd464b2721cdbb557c61972d99bbcb9ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("par_impar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("par_impar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LPRaula7/Matriz/sala/obj/Debug/net9.0/sala.AssemblyInfo.cs
+++ LPRaula7/Matriz/sala/obj/Debug/net9.0/sala.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sala")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd268aeef1bef9521388bba7dfc3b3d9f2f53d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de244c5fd464b2721cdbb557c61972d99bbcb9ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("sala")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sala")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
